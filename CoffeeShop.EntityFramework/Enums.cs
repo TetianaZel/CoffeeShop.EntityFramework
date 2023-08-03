d43ec1e92ccf9d0ef35910fec0326238f9d042b9
Tetiana Zelepukina <tetiana.zelepukina@sycous.com>
2023-08-03 20:17:52 +0100

    Add project files.

---
+++ CoffeeShop.EntityFramework/Enums.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.EntityFramework
+{
+    internal class Enums
+    {
+        internal enum MenuOptions
+        {
+            AddCategory,
+            ViewAllCategories,
+            AddProduct,
+            DeleteProduct,
+            UpdateProduct,
+            ViewProduct,
+            ViewAllProducts,
+            Quit
+        }
+
+    }
+}
